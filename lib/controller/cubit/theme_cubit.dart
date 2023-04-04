6a2db38df8b2fe19cd0e75e64f652e6a5ebe770b
Danushan Ravendran <it20051648@my.sliit.lk>
2023-04-04 20:51:15 +0530

    add cubit

---
+++ lib/controller/cubit/theme_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'theme_state.dart';
+
+class ThemeCubit extends Cubit<ThemeState> {
+  ThemeCubit() : super(ThemeInitial());
+
+  bool _isDark = false;
+  bool get isDark => _isDark;
+
+  void changeTheam() {
+    _isDark = !_isDark;
+  }
+
+  emit(ThemeChanged) {}
+}
